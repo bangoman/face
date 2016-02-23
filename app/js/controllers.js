e880dae9c624247c3a47f339314a3526ec307429
gahillel <loolligarmi@gmail.com>
2016-02-23 19:02:58 +0200

    adding material design

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -98,7 +98,7 @@ app.controller('faceCtrl', ['$scope', '$window', '$mdSidenav', 'Facebook','filte
             $scope.patCanvas.height = _video.height;
             $scope.ctxPat = $scope.patCanvas.getContext('2d');
             $scope.ctxPat.drawImage(_video, 0, 0, _video.width, _video.height);
-    //        $("#picture").attr('src', $scope.patCanvas.toDataURL("image/jpeg"));
+            $("#picture").attr('src', $scope.patCanvas.toDataURL("image/jpeg"));
 
 
             $scope.testMask = new Image();
