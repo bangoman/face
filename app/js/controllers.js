4170eafb310c04806db6258760c126b46e455b31
gahillel <loolligarmi@gmail.com>
2016-02-23 10:02:59 +0200

    adding material design

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -26,28 +26,19 @@ app.controller('faceCtrl', ['$scope', function($scope) {
             $("#picture").attr('src', $scope.patCanvas.toDataURL("image/jpeg"));
             $scope.srcTest = $("#picture").src;
             console.log("$scope.srcTest = ", $scope.srcTest);
-            setTimeout(function () {
-                console.log("#picture = ",  $scope.patCanvas.toDataURL("image/jpeg"));
-                setTimeout(function () {
-                    $scope.mergeImages();
-                }, 30);
-            },30 );
-            console.log($scope.face);
+            $scope.mergeImages();
         }
     };
 
     $scope.mergeImages = function(){
 		$scope.showWebcam = false;
-        console.log($('#picture').src, $scope.srcTest);
+        console.log($('#picture'));
     	$("#picture").faceDetection({
             complete: function (faces) {
-                console.log(faces[0]);
                 var x = faces[0].width/($("#mask").width()/5.5);
                 var y = faces[0].height/($("#mask").height()/4.4);
-                console.log(x);
                 var ctx = document.getElementById("canvas").getContext("2d");
                 $scope.face.addEventListener("load", function() {
-                    console.log($scope.face);
                     ctx.drawImage($scope.face,   $("#mask").width()/1.69 - (faces[0].width/x) - faces[0].x/x ,($("#mask").height()/3.2) - (faces[0].y/x),
                         $('#picture').width()/x, $('#picture').height()/y);
 
